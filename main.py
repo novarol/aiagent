b08da86c792e5a4731bea27c414bcf0d874ed76a
novarol <novarol3d@gmail.com>
2026-01-16 16:29:18 +0100

    feat: add cli arguments user prompt and verbose

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from argparse import ArgumentParser
 
 def main():
+    parser = ArgumentParser(description="Gemini Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    user_prompt = args.user_prompt
+    is_verbose = args.verbose
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -10,16 +20,20 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
 
     if not response.usage_metadata:
         raise RuntimeError("Gemini failed to respond. Try again.")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print("Response:")
     print(response.text)
