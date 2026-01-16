4acddc105d56c82aac27f4cbecebd3573ae81d20
novarol <novarol3d@gmail.com>
2026-01-16 18:23:19 +0100

    ref: extract methods

--- main.py
+++ main.py
@@ -5,38 +5,67 @@ from google.genai import types
 from argparse import ArgumentParser
 
 def main():
+    user_prompt, is_verbose = get_cli_arguments()
+
+    prompt_client()(user_prompt, is_verbose)
+
+
+def get_cli_arguments() -> tuple[str, bool]:
     parser = ArgumentParser(description="Gemini Agent")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-    user_prompt = args.user_prompt
-    is_verbose = args.verbose
+    return args.user_prompt, args.verbose
 
+
+def get_environment_variable(name: str):
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    if not api_key:
-        raise RuntimeError("API key not found! Make sure GEMINI_API_KEY is set as an environment variable!")
 
-    client = genai.Client(api_key=api_key)
+    env_var = os.environ.get("GEMINI_API_KEY")
 
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    if not env_var:
+        raise RuntimeError(f"Environment variable not found! Make sure {name} is set!")
+    
+    return env_var
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages
-    )
 
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini failed to respond. Try again.")
+def create_client():
+    api_key = get_environment_variable("GEMINI_API_KEY")
 
-    if is_verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    return genai.Client(api_key=api_key)
 
-    print("Response:")
-    print(response.text)
+
+def prompt_client():
+    client = create_client()
+    messages = []
+
+    def add_content(role: str, text: str):
+        messages.append(types.Content(role=role, parts=[types.Part(text=text)]))
+
+    def generate_content(user_prompt: str, is_verbose: bool):
+        if is_verbose:
+            print(f"User prompt: {user_prompt}")
+
+        add_content("user", user_prompt)
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages
+        )
+
+        if not response.usage_metadata:
+            raise RuntimeError("Gemini failed to respond. Try again.")
+
+        if is_verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        print("Response:")
+        print(response.text)
+
+    return generate_content
+
 
 
 if __name__ == "__main__":
